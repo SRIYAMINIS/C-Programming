9a220d53212d6ae367127f95a06ad247d1bb2a67
SRIYAMINIS <34641544+SRIYAMINIS@users.noreply.github.com>
2017-12-18 09:07:07 +0000

    Create size of.c

---
+++ Beginner level/size of.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+   int a=10;
+   char b='r';
+   float c=0.16;
+   printf("size of %d\n",sizeof(a));
+   printf("size of %d\n",sizeof(b));
+   printf("size of %d\n",sizeof(c));
+	return 0;
+}
