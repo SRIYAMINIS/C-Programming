71399f7fb6e4a22c9eaf52c99f108929cc6c9b94
SRIYAMINIS <34641544+SRIYAMINIS@users.noreply.github.com>
2017-12-19 10:56:02 +0000

    Create arrayfunction.c

---
+++ Hunter level/arrayfunction.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void){
+	int a[10],i,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	int ele=max(a,n);
+	printf("%d",ele);
+	return 0;
+}
+int max(int b[],int x){
+	int c=b[0];
+	for(int i=1;i<x;i++){
+		if(c<b[i]){
+			c=b[i];
+		}
+	}
+	return c;
+}
