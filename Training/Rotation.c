f049fef2124a0010c3ded99aa7138b52b568abc1
SRIYAMINIS <34641544+SRIYAMINIS@users.noreply.github.com>
2017-12-20 14:52:23 +0530

    Create Rotation.c

---
+++ Training/Rotation.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+int a[10],i,j,n,k,temp;
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++){
+	scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++){
+	temp=a[0];
+	for(j=0;j<n;j++){
+		a[j]=a[j+1];
+	}
+	a[n-1]=temp;
+}
+for(i=0;i<n;i++){
+	printf("%d\n",a[i]);
+	
+}
+	return 0;
+}
